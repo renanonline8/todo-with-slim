ed686223a89af37334e052ea7c0e9f0e2df31e05
Renan Santos Gomes <renanonline8@gmail.com>
2019-01-22 22:01:42 -0200

    #2 Criado recurso de cadastro de nova tarefa
    Adicionado recurso para cadastrar nova tarefa

--- src/App/Controller/ControllerTask.php
+++ src/App/Controller/ControllerTask.php
@@ -3,10 +3,14 @@ namespace App\Controller;
 
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
+use Respect\Validation\Validator as v;
 
 use Utils\Controller\Controller;
 
 use App\CSRF\ArrayCSRF;
+use App\Validacao\ValidacaoRedireciona;
+use App\Models\Task;
+use App\Models\Usuario;
 
 /**
  * Controller de tarefas
@@ -26,7 +30,53 @@ final class ControllerTask extends Controller
         //Dados da sessão
         $sessao = $this->twigArgs->retArgs()['sessao'];
 
-        
-        $task = $request->getParam('new_task');
+        //Usuario ID
+        $id = $sessao['id'];
+
+        //Tarefa
+        $taskName = $request->getParam('new_task');
+
+        //Validar
+        $validacao = new ValidacaoRedireciona(
+            $this->router->pathFor('dashboard')
+        );
+        $validacao->adicionaRegra(
+            v::stringType()
+            ->notEmpty()
+            ->validate($id),
+            15
+        );
+        $validacao->adicionaRegra(
+            v::stringType()
+            ->notEmpty()
+            ->validate($taskName),
+            16
+        );
+        $validacao->adicionaRegra(
+            v::stringType()
+            ->length(null, 255)
+            ->validate($taskName),
+            17
+        );
+        if (!$validacao->valida()) {
+            return $response->withRedirect(
+                $validacao->retornaURLErros()
+            );
+        }
+
+        //Localizar usuario
+        $user = Usuario::find_by_id_externo($id);
+
+        //Adicionar tarefa no banco
+        $task = new Task();
+        $task->id_ext = uniqid();
+        $task->task = $taskName;
+        $task->id_user = $user->id;
+        $task->save();
+
+        return $response->withRedirect(
+            $this->router->pathFor('dashboard') .
+            '?mensagens=18'
+        );
     }
 }
---
+++ src/App/Models/Task.php
@@ -0,0 +1,9 @@
+<?php
+namespace App\Models;
+
+use \ActiveRecord\Model as Model;
+
+final class Task extends Model
+{
+    
+}
