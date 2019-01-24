c82b0711aab8eb8bd877cbbbc34de06f07037329
Renan Santos Gomes <renanonline8@gmail.com>
2019-01-23 22:02:06 -0200

    #3 Listar tarefas não concluídas no Dashboard

--- src/App/Controller/ControllerDashboard.php
+++ src/App/Controller/ControllerDashboard.php
@@ -1,21 +1,53 @@
 <?php
 namespace App\Controller;
 
-use Utils\Controller\Controller;
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
+
+use Utils\Controller\Controller;
+
 use App\CSRF\ArrayCSRF;
+use App\Models\Usuario;
+use App\Models\Task;
 
 final class ControllerDashboard extends Controller
 {
     public function dash(Request $request, Response $response, Array $args)
     {
+        //CSRF
         $csrf = new ArrayCSRF(
             $this->csrf->getTokenNameKey(),
             $this->csrf->getTokenValueKey(),
             $request
         );
         $this->twigArgs->adcDados('csrf', $csrf->getCSRF());
+
+        //Dados da sessão
+        $sessao = $this->twigArgs->retArgs()['sessao'];
+
+        //Usuario ID
+        $id = $sessao['id'];
+
+        //Localizar id do usuário
+        $user = Usuario::find_by_id_externo($id);
+        
+        //Obter tarefas do usuário
+        $tasks = Task::find_all_by_id_user_and_checked($user->id, false);
+
+        //Converter para array as tarefas
+        $listTasks = [];
+        foreach ($tasks as $key => $value) {
+            array_push($listTasks, $value->to_array(
+                array(
+                    'only' => array('id_ext', 'task')
+                )
+            ));
+        }
+
+        //Adicionar ao twig
+        $this->twigArgs->adcDados('tasksList', $listTasks);
+
+        //Retornar view
         return $this->view->render($response, 'dashboard.twig', $this->twigArgs->retArgs());
     }
 }
